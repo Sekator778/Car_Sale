5eacc1ca86280d90aa31729a5cfd54d310eb7962
Sekator778 <sekator778@gmail.com>
2020-06-30 16:13:49 +0300

    test +

--- src/main/test/java/ConnectorTest.java
+++ src/main/test/java/ConnectorTest.java
@@ -1,5 +1,5 @@
-package connection;
-
+import connection.Connection;
+import connection.Connector;
 import models.Car;
 import models.User;
 import org.junit.Test;
@@ -12,18 +12,14 @@ import java.util.*;
 import static org.junit.Assert.*;
 
 public class ConnectorTest {
-
     private final Connection connector = Connector.getINSTANCE();
     private final Service service = ServiceIml.getInstance();
-    private final EntityManagerFactory emf = Persistence
-            .createEntityManagerFactory("SaleCar");
-
-
+    private final EntityManagerFactory emf = Persistence.createEntityManagerFactory("SaleCar");
     @Test
     public void whenAddUserThenAdded() {
-        User user = new User("Tom", "tom");
-        int userId = this.connector.addUser(user);
-        EntityManager em = this.emf.createEntityManager();
+        User user = new User("User", "pass");
+        int userId = connector.addUser(user);
+        EntityManager em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         Query q = em.createQuery("select u From User u where u.id = :userId");
@@ -32,27 +28,24 @@ public class ConnectorTest {
         tx.commit();
         assertEquals(foundUser.getName(), (user.getName()));
         em.close();
-
     }
-
     @Test
     public void whenIsCredentialThenTrue() {
-        User user = new User("Tom", "tom");
-        int userId = this.connector.addUser(user);
-        User foundUser = this.connector.isCredential(user);
+        User user = new User("Alex", "pass");
+        int userId = connector.addUser(user);
+        User foundUser = connector.isCredential(user);
         assertEquals(foundUser.getId(), userId);
     }
-
     @Test
     public void whenChangeStatusThenChanged() {
-        User user = new User("Tom", "tom");
-        int userId = this.connector.addUser(user);
+        User user = new User("Alex", "pass");
+        int userId = connector.addUser(user);
         user.setId(userId);
-        Car car = new Car("type", "brand", "model", 100, 2020, "desc", 10000);
+        Car car = new Car("type", "brand", "model", 1, 2020, "desc", 123);
         car.setUser(user);
-        int carId = this.connector.addCar(car);
-        this.connector.changeStatus(car);
-        EntityManager em = this.emf.createEntityManager();
+        int carId = connector.addCar(car);
+        connector.changeStatus(car);
+        EntityManager em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         Query q = em.createQuery("select c From Car c where c.id = :carId");
@@ -62,16 +55,15 @@ public class ConnectorTest {
         assertEquals(car.isSold(), foundCar.isSold());
         em.close();
     }
-
     @Test
     public void whenAddCarThenAdded() {
-        User user = new User("Tom", "tom");
-        int userId = this.connector.addUser(user);
+        User user = new User("Alex", "Alex");
+        int userId = connector.addUser(user);
         user.setId(userId);
         Car car = new Car("type", "brand", "model", 100, 2020, "desc", 10000);
         car.setUser(user);
-        int carId = this.connector.addCar(car);
-        EntityManager em = this.emf.createEntityManager();
+        int carId = connector.addCar(car);
+        EntityManager em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         Query q = em.createQuery("select c From Car c where c.id = :carId");
@@ -84,67 +76,67 @@ public class ConnectorTest {
 
     @Test
     public void whenCarsByUserThenCarsList() {
-        User user = new User("Tom", "tom");
-        int userId = this.connector.addUser(user);
-        User user2 = new User("Sam", "sam");
-        int user2Id = this.connector.addUser(user2);
+        User user = new User("Alex", "Alex");
+        int userId = connector.addUser(user);
+        User user2 = new User("Bob", "Bob");
+        int user2Id = connector.addUser(user2);
         user.setId(userId);
         user2.setId(user2Id);
         Car car = new Car("type", "brand", "model", 100, 2020, "desc", 10000);
         car.setUser(user);
         Car car2 = new Car("type2", "brand2", "model2", 102, 2022, "desc2", 10002);
         car2.setUser(user2);
-        this.connector.addCar(car);
-        this.connector.addCar(car2);
-        List<Car> carsByUser2 = this.connector.carsByUser(user2);
+        connector.addCar(car);
+        connector.addCar(car2);
+        List<Car> carsByUser2 = connector.carsByUser(user2);
         List<Car> expected = List.of(car2);
         assertEquals(expected, carsByUser2);
     }
 
     @Test
     public void whenAllCarsThenAll() {
-        User user = new User("Tom", "tom");
-        int userId = this.connector.addUser(user);
-        User user2 = new User("Sam", "sam");
-        int user2Id = this.connector.addUser(user2);
+        User user = new User("Alex", "Alex");
+        int userId = connector.addUser(user);
+        User user2 = new User("Bob", "Bob");
+        int user2Id = connector.addUser(user2);
         user.setId(userId);
         user2.setId(user2Id);
         Car car = new Car("type", "brand", "model", 100, 2020, "desc", 10000);
         car.setUser(user);
         Car car2 = new Car("type2", "brand2", "model2", 102, 2022, "desc2", 10002);
         car2.setUser(user2);
-        this.connector.addCar(car);
-        this.connector.addCar(car2);
-        List<Car> allCars = this.connector.allCars();
+        connector.addCar(car);
+        connector.addCar(car2);
+        List<Car> allCars = connector.allCars();
         List<Car> expected = List.of(car, car2);
         assertEquals(expected, allCars);
     }
 
     @Test
     public void whenAllBrandsThenAll() {
-        User user = new User("Tom", "tom");
-        int userId = this.connector.addUser(user);
-        User user2 = new User("Sam", "sam");
-        int user2Id = this.connector.addUser(user2);
+        User user = new User("Alex", "Alex");
+        int userId = connector.addUser(user);
+        User user2 = new User("Bob", "Bob");
+        int user2Id = connector.addUser(user2);
         user.setId(userId);
         user2.setId(user2Id);
         Car car = new Car("type", "brand", "model", 100, 2020, "desc", 10000);
         car.setUser(user);
         Car car2 = new Car("type2", "brand2", "model2", 102, 2022, "desc2", 10002);
         car2.setUser(user2);
-        this.connector.addCar(car);
-        this.connector.addCar(car2);
-        List<String> allBrands = this.connector.allBrands();
+        connector.addCar(car);
+        connector.addCar(car2);
+        List<String> allBrands = connector.allBrands();
         List<String> expected = List.of(car.getBrand(), car2.getBrand());
         assertEquals(expected, allBrands);
     }
 
     @Test
     public void whenDayFilterThenTodayTickets() {
-        User user = new User("Tom", "tom");
-        int userId = this.connector.addUser(user);
-        User user2 = new User("Sam", "sam");
-        int user2Id = this.connector.addUser(user2);
+        User user = new User("Alex", "Alex");
+        int userId = connector.addUser(user);
+        User user2 = new User("Bob", "Bob");
+        int user2Id = connector.addUser(user2);
         user.setId(userId);
         user2.setId(user2Id);
         Car car = new Car("type", "brand", "model", 100, 2020, "desc", 10000);
@@ -156,19 +148,19 @@ public class ConnectorTest {
         car2.setDate(cal);
         car2.setUser(user2);
 
-        this.connector.addCar(car);
-        this.connector.addCar(car2);
-        List<Car> todayCars = this.service.filter(true, false, "none");
+        connector.addCar(car);
+        connector.addCar(car2);
+        List<Car> todayCars = service.filter(true, false, "none");
         List<Car> expected = List.of(car);
         assertEquals(expected, todayCars);
     }
 
     @Test
     public void whenPhotoFilterThenOnlyWithPhoto() {
-        User user = new User("Tom", "tom");
-        int userId = this.connector.addUser(user);
-        User user2 = new User("Sam", "sam");
-        int user2Id = this.connector.addUser(user2);
+        User user = new User("Alex", "Alex");
+        int userId = connector.addUser(user);
+        User user2 = new User("Bob", "Bob");
+        int user2Id = connector.addUser(user2);
         user.setId(userId);
         user2.setId(user2Id);
         Car car = new Car("type", "brand", "model", 100, 2020, "desc", 10000);
@@ -176,38 +168,38 @@ public class ConnectorTest {
         Car car2 = new Car("type2", "brand2", "model2", 102, 2022, "desc2", 10002);
         car2.setUser(user2);
         car2.setPicture("some picture");
-        this.connector.addCar(car);
-        this.connector.addCar(car2);
-        List<Car> photoCars = this.service.filter(false, true, "none");
+        connector.addCar(car);
+        connector.addCar(car2);
+        List<Car> photoCars = service.filter(false, true, "none");
         List<Car> expected = List.of(car2);
         assertEquals(expected, photoCars);
     }
 
     @Test
     public void whenBrandFilterThenOnlyOne() {
-        User user = new User("Tom", "tom");
-        int userId = this.connector.addUser(user);
-        User user2 = new User("Sam", "sam");
-        int user2Id = this.connector.addUser(user2);
+        User user = new User("Alex", "Alex");
+        int userId = connector.addUser(user);
+        User user2 = new User("Bob", "Bob");
+        int user2Id = connector.addUser(user2);
         user.setId(userId);
         user2.setId(user2Id);
         Car car = new Car("type", "brand", "model", 100, 2020, "desc", 10000);
         car.setUser(user);
         Car car2 = new Car("type2", "brand2", "model2", 102, 2022, "desc2", 10002);
         car2.setUser(user2);
-        this.connector.addCar(car);
-        this.connector.addCar(car2);
-        List<Car> brandCars = this.service.filter(false, false, "brand");
+        connector.addCar(car);
+        connector.addCar(car2);
+        List<Car> brandCars = service.filter(false, false, "brand");
         List<Car> expected = List.of(car);
         assertEquals(expected, brandCars);
     }
 
     @Test
     public void whenAllFiltersThenThird() {
-        User user = new User("Tom", "tom");
-        int userId = this.connector.addUser(user);
-        User user2 = new User("Sam", "sam");
-        int user2Id = this.connector.addUser(user2);
+        User user = new User("Alex", "Alex");
+        int userId = connector.addUser(user);
+        User user2 = new User("Bob", "Bob");
+        int user2Id = connector.addUser(user2);
         user.setId(userId);
         user2.setId(user2Id);
         Car car = new Car("type", "brand", "model", 100, 2020, "desc", 10000);
@@ -222,12 +214,11 @@ public class ConnectorTest {
         car.setDate(cal);
         car.setPicture("some picture");
         car3.setPicture("new picture");
-        this.connector.addCar(car);
-        this.connector.addCar(car2);
-        this.connector.addCar(car3);
-        List<Car> cars = this.service.filter(true, true, "brand2");
+        connector.addCar(car);
+        connector.addCar(car2);
+        connector.addCar(car3);
+        List<Car> cars = service.filter(true, true, "brand2");
         List<Car> expected = List.of(car3);
         assertEquals(expected, cars);
     }
-
 }
