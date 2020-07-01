657a3b0ec7eb293c0852d39904dca0435d675ca5
Sekator778 <sekator778@gmail.com>
2020-07-01 18:36:53 +0300

    test +

--- src/main/java/servlets/StartPageServlet.java
+++ src/main/java/servlets/StartPageServlet.java
@@ -1,8 +1,41 @@
 package servlets;
 
-/**
- *
- */
+import models.Car;
+import service.Service;
+import service.ServiceIml;
 
-public class StartPageServlet {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * the servlet for loading the main table for the main page.
+ */
+public class StartPageServlet extends HttpServlet {
+
+    private Service service = ServiceIml.getInstance();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Hi all ++++++++++++++++++++++++=");
+        HttpSession session = req.getSession();
+        String name = (String) session.getAttribute("name");
+        if (name == null) {
+            name = "Log In";
+        }
+        List<Car> list = this.service.loadTable();
+        List<String> brands = this.service.allBrands();
+        req.setAttribute("list", list);
+        req.setAttribute("name", name);
+        req.setAttribute("day", false);
+        req.setAttribute("photo", false);
+        req.setAttribute("setBrand", "none");
+        req.setAttribute("brands", brands);
+        getServletContext().getRequestDispatcher("/table.jsp").forward(req, resp);
+    }
 }
+
